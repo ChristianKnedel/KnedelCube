e8e9d18c42a584b4b25b17c831c904479f6a3a76
Administrator <himself@christian-knedel.de>
2024-05-19 10:58:28 +0200

    I have documented my code better

--- merge-one-gif.sh
+++ merge-one-gif.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
-RANDOM_DIR=$(find sorted/ -type d | shuf -n 1)
+HOME_BASE=/home/cube/knedelcube/
+
+# My animations are organized by frames  "images/sorted-by-frame"
+RANDOM_DIR=$(find "${HOME_BASE}/images/" -type d | shuf -n 1)
 WORK_DIR=/tmp/knedelcube/
 
-if [ ! -d "${WORK_DIR}" ] 
+if [ ! -d "${WORK_DIR}" ]
 then
     mkdir -p "${WORK_DIR}"
 fi
 rm -rf "${WORK_DIR}/*"
+
 cp ${RANDOM_DIR}/*.gif "${WORK_DIR}/"
 
 select_site() {
     random_image=$(ls ${WORK_DIR}/*.gif | shuf -n 1)
+
+    #mark animation
     mv "$random_image" "$random_image.selected"
     echo "$random_image.selected"
 }
 
-image1=$(select_site)
-image2=$(select_site)
-image3=$(select_site)
-image4=$(select_site)
-image5=$(select_site)
+#Each animation is one side on the cube
+animation1=$(select_site)
+animation2=$(select_site)
+animation3=$(select_site)
+animation4=$(select_site)
+animation5=$(select_site)
 
-ffmpeg -y -i $image1 -i $image2 -i $image3 -i $image4 -i $image5 -filter_complex hstack=inputs=5 merged.gif
+
+#merge to one animation
+ffmpeg  -y -i $animation1 -i $animation2 -i $animation3 -i $animation4 -i $animation5 -filter_complex hstack=inputs=5 -color_range 8  "${HOME_BASE}/merged.gif"
+
+#change sides
+kill $(ps -aux | grep gif-viewer.py | awk '{print $2}')
+nice python "${HOME_BASE}/gif-viewer.py" "${HOME_BASE}/merged.gif" 1> /dev/null 2> /dev/null &
